81996ba5a5c96aa85ee35deac82b221d05bd5843
ryan.muktiadhi <ryan.muktiadhi@mii.co.id>
2025-07-24 15:13:01 +0700

    shopee V.0.5

--- src/backend/database/client.ts
+++ src/backend/database/client.ts
@@ -1,6 +1,7 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { createRequire } from 'module';
+import { logInfo } from '../utils/logger';
 
 const require = createRequire(import.meta.url);
 const knex = require('knex');
@@ -8,8 +9,8 @@ const knex = require('knex');
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const dbPath = path.join(__dirname, 'data', 'admindb.sqlite');
-
+const dbPath = path.join(__dirname, 'data', 'admdb.sqlite');
+logInfo("INI ALAMAT TABLENYA : ",dbPath)
 const db = knex({
   client: 'better-sqlite3',
   connection: {
