8e654852b9e3379ba625c7cdeb2c8a278e9c43da
ryan.muktiadhi <ryan.muktiadhi@mii.co.id>
2025-07-24 22:16:15 +0700

    database change V.0.1

--- src/backend/repositories/shopee.repository.ts
+++ src/backend/repositories/shopee.repository.ts
@@ -3,15 +3,34 @@ import { logInfo } from '../utils/logger';
 
 export class ShopeeRepository {
   async saveQShopee(payload:any, userInfo:any) {
+    // Pastikan fromdate diformat jadi YYYY-MM-DD
+    const formattedDate = new Date(payload.fromdate).toISOString().substring(0, 10); // hasilnya "2025-07-24"
+
+    logInfo("Data datepick : ",formattedDate)
     const query = await db('q_shopee').insert(
         {
           fromtime: payload.fromtime,
           totime: payload.totime,
           created_by: userInfo.iduser,
           created_at: db.fn.now(),
-          datepick: payload.fromdate
+          datepick: formattedDate
         }
       ).returning('id');
+
     return await query;
+  }
+  async selectQShopeeAll() {
+    const result = await db.select([
+      'qs.id',
+      'qs.datepick',
+      'qs.fromtime',
+      'qs.totime',
+      'qs.created_by',
+      'mu.fullname',
+      'qs.created_at'
+    ]).from('q_shopee as qs').leftJoin("m_user as mu","qs.created_by","mu.iduser").orderBy("qs.created_at","desc");
+    return result;
+
+    // return await query;
   }
 }
--- src/backend/services/api.service.ts
+++ src/backend/services/api.service.ts
@@ -9,6 +9,14 @@ export class ApiService {
   async qShopeeInsert(payload: any, userinfo: any) {
     const shopeeResult = await this.shopeeRepo.saveQShopee(payload,userinfo);
     if(!shopeeResult) return ApiResponse.successNoData(shopeeResult,"Unable to insert data!");
-    return ApiResponse.success(shopeeResult,"Records found");
+    //################## Berhasil Isi #######################
+    // if(shopeeResult){
+      const rowQueryShopee = await this.shopeeRepo.selectQShopeeAll();
+      if(!rowQueryShopee) {
+        return ApiResponse.successNoData(shopeeResult,"Unable to get data!");
+      } else {
+        return ApiResponse.success(rowQueryShopee,"Records found");
+      }
+    // }
   }
 }
